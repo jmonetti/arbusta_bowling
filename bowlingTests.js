41387cec1672a481239dc686e0a39c52b2f92475
Juan Monetti <juanmonetti@gmail.com>
2019-05-08 23:10:39 -0300

    BDD exercise

---
+++ bowlingTests.js
@@ -0,0 +1,51 @@
+describe("Escenario: 01 Juego Nulo", function() {    
+	describe("Dado que el juego recien empiza", function() {        
+        beforeEach(function() { 
+			juegoBowling = new Game();
+		});
+	
+        describe("Cuando tiro 20 veces 0 pinos", function () {
+            beforeEach(function() {
+                tirarPinos(juegoBowling, 20, 0);
+	        });  
+	
+            it("Entonces el resultado es 0", function() {
+                expect(juegoBowling.getScore()).toBe(0);
+            });
+        });  
+    });
+});
+
+describe("Escenario: 02 Juego Simple todos unos", function() {
+    
+	describe("Dado que el juego recien empiza", function() {
+        beforeEach(function() {
+            juegoBowling = new Game();
+        });
+	
+        describe("Cuando tiro 20 veces 1 pino", function () {
+            beforeEach(function() {
+                tirarPinos(juegoBowling, 20, 1);
+	        });  
+	
+            it("Entonces el resultado es 20", function() {
+                expect(juegoBowling.getScore()).toBe(20);
+            });
+        });  
+    });
+});
+
+// instancia del juego de Bowling
+var juegoBowling = null;
+
+/*
+Funcion tirarPinos
+- juegoBowling: una instancia del juego de bowling
+- cantTiros: la cantidad de veces que vamos a tirar
+- pinos: la cantidad de pinos tirados por tiro 
+*/
+function tirarPinos(juegoBowling, cantTiros, pinos) {
+	for(i = 0; i < cantTiros; i++) {
+		juegoBowling.roll(pinos);
+	}
+};
